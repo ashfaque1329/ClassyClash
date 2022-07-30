1113e2152b5af5fe8f5429c25073f6a4bb924432
Ashfaque Chowdhury <asfaque1329@gmail.com>
2022-07-30 23:49:16 +0530

    v1.0.0

--- main.cpp
+++ main.cpp
@@ -1,45 +1,110 @@
 #include "raylib.h"
 #include "raymath.h"
+#include "Character.h"
+#include "Prop.h"
+#include "Enemy.h"
+#include <string>
 
-int main() {
+int main()
+{
+    const int windowWidth{800};
+    const int windowHeight{600};
+    InitWindow(windowWidth, windowHeight, "Ashfaque's Classy Clash");
 
-    const int window_width{1080};
-    const int window_height{800};
-    const char *title{"Classy Clash"};
-    const int FPS{60};
+    Texture2D map = LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos{0.0, 0.0};
+    const float mapScale{4.0f};
 
-    InitWindow(window_width,window_height,title);
-    SetTargetFPS(FPS);
+    Character knight{windowWidth, windowHeight};
 
-    Texture2D map{LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/nature_tileset/OpenWorldMap24x24.png")};
-    Vector2 map_position{0.0,0.0};
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/nature_tileset/Log.png")}};
 
-    const float speed{4.0};
+    Enemy goblin{
+        Vector2{800.f, 300.f},
+        LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/characters/goblin_idle_spritesheet.png"),
+        LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/characters/goblin_run_spritesheet.png")};
 
-    while(!WindowShouldClose()){
+    Enemy slime{
+        Vector2{500.f, 700.f},
+        LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/characters/slime_idle_spritesheet.png"),
+        LoadTexture("/home/ashfaque/CLionProjects/C++/GameProgramming/ClassyClash/characters/slime_run_spritesheet.png")};
+
+    Enemy *enemies[]{
+        &goblin,
+        &slime};
+
+    for (auto enemy : enemies)
+    {
+        enemy->setTarget(&knight);
+    }
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 direction{};
-        if(IsKeyDown(KEY_A)) direction.x-=1.0;
-        if(IsKeyDown(KEY_D)) direction.x+=1.0;
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
-        if(IsKeyDown(KEY_W)) direction.y-=1.0;
-        if(IsKeyDown(KEY_S)) direction.y+=1.0;
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        if(Vector2Length(direction)!=0.0){
-            map_position= Vector2Subtract(map_position, Vector2Scale(Vector2Normalize(direction),speed));
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
         }
 
+        if (!knight.getAlive()) // Character is not alive
+        {
+            DrawText("Game Over!", 55.f, 45.f, 40, RED);
+            EndDrawing();
+            continue;
+        }
+        else // Character is alive
+        {
+            std::string knightsHealth = "Health: ";
+            knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+            DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
+        }
 
-        DrawTextureEx(map,map_position,0.0,4.0,WHITE);
+        knight.tick(GetFrameTime());
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > map.width * mapScale ||
+            knight.getWorldPos().y + windowHeight > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
+
+        for (auto enemy : enemies)
+        {
+            enemy->tick(GetFrameTime());
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            for (auto enemy : enemies)
+            {
+                if (CheckCollisionRecs(enemy->getCollisionRec(), knight.getWeaponCollisionRec()))
+                {
+                    enemy->setAlive(false);
+                }
+            }
+        }
 
         EndDrawing();
-
     }
-
     CloseWindow();
-
-
-    return 0;
 }
